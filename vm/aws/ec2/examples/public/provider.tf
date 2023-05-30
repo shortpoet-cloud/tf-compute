5bbc014bae85687acfa5976d6a6ef7a3835a2d3a
Carlos Soriano <soriano.carlitos@gmail.com>
2023-05-29 20:18:16 -0400

    bump tf version

--- vm/aws/ec2/examples/public/provider.tf
+++ vm/aws/ec2/examples/public/provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 4.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
 }
 
 # Configure the AWS Provider
--- vm/gcs/main.tf
+++ vm/gcs/main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "4.52.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
 }
 
 provider "google" {
